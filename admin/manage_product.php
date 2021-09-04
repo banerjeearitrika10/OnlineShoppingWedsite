03154ceaca501fd1aefcfa0693092a51228a5471
banerjeearitrika10 <78617690+banerjeearitrika10@users.noreply.github.com>
2021-09-04 21:28:38 +0530

    Add files via upload

---
+++ admin/manage_product.php
@@ -0,0 +1,147 @@
+<?php
+require('top.inc.php');
+$categories_id='';
+$name='';
+$price='';
+$qty='';
+$image='';
+$short_desc	='';
+
+$msg='';
+$image_required='required';
+if(isset($_GET['id']) && $_GET['id']!=''){
+	$image_required='';
+	$id=get_safe_value($con,$_GET['id']);
+	$res=mysqli_query($con,"select * from product where id='$id'");
+	$check=mysqli_num_rows($res);
+	if($check>0){
+		$row=mysqli_fetch_assoc($res);
+		$categories_id=$row['categories_id'];
+		$name=$row['p_name'];
+		$price=$row['price'];
+		$qty=$row['qty'];
+		$short_desc=$row['short_desc'];
+	}else{
+		header('location:product.php');
+		die();
+	}
+}
+
+if(isset($_POST['submit'])){
+	$categories_id=get_safe_value($con,$_POST['categories_id']);
+	$name=get_safe_value($con,$_POST['p_name']);
+	$price=get_safe_value($con,$_POST['price']);
+	$qty=get_safe_value($con,$_POST['qty']);
+	$short_desc=get_safe_value($con,$_POST['short_desc']);
+	
+	$res=mysqli_query($con,"select * from product where p_name='$name'");
+	$check=mysqli_num_rows($res);
+	if($check>0){
+		if(isset($_GET['id']) && $_GET['id']!=''){
+			$getData=mysqli_fetch_assoc($res);
+			if($id==$getData['id']){
+			
+			}else{
+				$msg="Product already exist";
+			}
+		}else{
+			$msg="Product already exist";
+		}
+	}
+	
+	
+	if($_GET['id']==0){
+		if($_FILES['image']['type']!='image/png' && $_FILES['image']['type']!='image/jpg' && $_FILES['image']['type']!='image/jpeg'){
+			$msg="Please select only png,jpg and jpeg image formate";
+		}
+	}else{
+		if($_FILES['image']['type']!=''){
+				if($_FILES['image']['type']!='image/png' && $_FILES['image']['type']!='image/jpg' && $_FILES['image']['type']!='image/jpeg'){
+				$msg="Please select only png,jpg and jpeg image formate";
+			}
+		}
+	}
+	
+	if($msg==''){
+		if(isset($_GET['id']) && $_GET['id']!=''){
+			if($_FILES['image']['name']!=''){
+				$image=rand(111111111,999999999).'_'.$_FILES['image']['name'];
+				move_uploaded_file($_FILES['image']['tmp_name'],'../media/product/'.$image);
+				$update_sql="update product set categories_id='$categories_id',p_name='$name',price='$price',qty='$qty',short_desc='$short_desc',image='$image' where id='$id'";
+			}else{
+				$update_sql="update product set categories_id='$categories_id',p_name='$name',price='$price',qty='$qty',short_desc='$short_desc' where id='$id'";
+			}
+			mysqli_query($con,$update_sql);
+		}else{
+			$image=rand(111111111,999999999).'_'.$_FILES['image']['name'];
+			move_uploaded_file($_FILES['image']['tmp_name'],'../media/product/'.$image);
+			mysqli_query($con,"insert into product(categories_id,p_name,price,qty,short_desc,status,image) values('$categories_id','$name','$price','$qty','$short_desc',1,'$image')");
+		}
+		header('location:product.php');
+		die();
+	}
+}
+?>
+<div class="content pb-0">
+            <div class="animated fadeIn">
+               <div class="row">
+                  <div class="col-lg-12">
+                     <div class="card">
+                        <div class="card-header"><strong>Product</strong><small> Form</small></div>
+                        <form method="post" enctype="multipart/form-data">
+							<div class="card-body card-block">
+							   <div class="form-group">
+									<label for="categories" class=" form-control-label">Categories</label>
+									<select class="form-control" name="categories_id">
+										<option>Select Category</option>
+										<?php
+										$res=mysqli_query($con,"select id,categories from categories order by categories asc");
+										while($row=mysqli_fetch_assoc($res)){
+											if($row['id']==$categories_id){
+												echo "<option selected value=".$row['id'].">".$row['categories']."</option>";
+											}else{
+												echo "<option value=".$row['id'].">".$row['categories']."</option>";
+											}
+											
+										}
+										?>
+									</select>
+								</div>
+								<div class="form-group">
+									<label for="categories" class=" form-control-label">Product Name</label>
+									<input type="text" name="p_name" placeholder="Enter product name" class="form-control" required value="<?php echo $name?>">
+								</div>					
+								<div class="form-group">
+									<label for="categories" class=" form-control-label">Price</label>
+									<input type="text" name="price" placeholder="Enter product price" class="form-control" required value="<?php echo $price?>">
+								</div>
+								
+								<div class="form-group">
+									<label for="categories" class=" form-control-label">Qty</label>
+									<input type="text" name="qty" placeholder="Enter qty" class="form-control" required value="<?php echo $qty?>">
+								</div>
+								
+								<div class="form-group">
+									<label for="categories" class=" form-control-label">Image</label>
+									<input type="file" name="image" class="form-control" <?php echo  $image_required?>>
+								</div>
+								
+								<div class="form-group">
+									<label for="categories" class=" form-control-label">Short Description</label>
+									<textarea name="short_desc" placeholder="Enter product short description" class="form-control" required><?php echo $short_desc?></textarea>
+								</div>
+							   <button id="payment-button" name="submit" type="submit" class="btn btn-lg btn-info btn-block">
+							   <span id="payment-button-amount">Submit</span>
+							   </button>
+							   <div class="field_error"><?php echo $msg?></div>
+							</div>
+						</form>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         
+<?php
+require('footer.inc.php');
+?>
